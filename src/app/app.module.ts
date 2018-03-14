92a985d7fbd0a21f4f5b3a09d3b1d9e1df21edc6
Thomas COTTIN <thom.cottin@gmail.com>
2018-03-14 14:09:44 +0100

    Add test component and implement crawler service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,21 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { TestComponent } from './test/test.component';
+import { DataCrawlerService } from './data-crawler.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [DataCrawlerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
