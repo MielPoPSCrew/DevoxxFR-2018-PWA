4b01b3792dbd21bcff56ed3f0560bb567cbc14cc
CottinThomas <thomas.cottin@spectrumgroupe.fr>
2018-04-04 21:07:42 +0200

    Change env variable declaration location

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,16 +2,5 @@ import { environment as env } from './environment'
 
 export const environment = {
   production: true,
-  ...env,
-  icons: {
-    conf: 'record_voice_over',
-    uni: 'school',
-    tia: 'build',
-    lab: 'developer_mode',
-    quick: 'timer',
-    bof: 'group_work',
-    key: 'label_outline',
-    ignite: 'whatshot',
-    other: 'chevron_right'
-  }
+  ...env
 };
