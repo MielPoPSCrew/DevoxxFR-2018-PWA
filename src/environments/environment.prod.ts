8673cb0ab9f4ef593b3dcf816d00d7407f4dc018
CottinThomas <thom.cottin@gmail.com>
2018-04-04 18:19:15 +0000

    Update environment.prod.ts : add environment variables for talks types icons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+import { environment as env } from './environment'
+
 export const environment = {
-  production: true
+  production: true,
+  ...env,
+  icons: {
+    conf: 'record_voice_over',
+    uni: 'school',
+    tia: 'build',
+    lab: 'developer_mode',
+    quick: 'timer',
+    bof: 'group_work',
+    key: 'label_outline',
+    ignite: 'whatshot',
+    other: 'chevron_right'
+  }
 };
