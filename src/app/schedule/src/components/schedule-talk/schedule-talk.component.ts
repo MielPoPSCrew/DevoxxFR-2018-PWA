6f3114bc2240ec2131a06955fa45b6b841c855f8
Clément Garin <clement.garin@outlook.fr>
2018-04-05 22:33:40 +0200

    Minor fixs in Schedule and adding favorite system

--- src/app/schedule/src/components/schedule-talk/schedule-talk.component.ts
+++ src/app/schedule/src/components/schedule-talk/schedule-talk.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { InfoModalComponent } from '../info-modal/info-modal.component';
+import { FavoriteTalksService } from '../../../../services/favorites.service';
+import { Talk } from '../../../../models/talk';
+
 @Component({
     selector: 'app-schedule-talk',
     templateUrl: './schedule-talk.component.html',
@@ -7,13 +10,22 @@ import { InfoModalComponent } from '../info-modal/info-modal.component';
 })
 export class ScheduleTalkComponent implements OnInit {
 
-    @Input('talk') talk: any;
+    @Input('talk') talk: Talk;
+    favorited: boolean;
 
-    constructor() { }
+    constructor(private favorites: FavoriteTalksService) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.favorited = this.favorites.isFavorite(this.talk.id);
+    }
 
     bookmarkTalk(talkId: string) {
-        console.log('bookmarkTalk', talkId);
+        if (this.favorites.isFavorite(talkId)) {
+            this.favorites.removeFavorite(talkId);
+            this.favorited = false;
+        } else {
+            this.favorites.addFavorite(talkId);
+            this.favorited = true;
+        }
     }
 }
