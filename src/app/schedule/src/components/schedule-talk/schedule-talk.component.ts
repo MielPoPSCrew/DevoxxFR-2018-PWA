e82fed02b543b0e2f0ca3f5fd4c8f3d34fbeffa8
Thomas COTTIN <thom.cottin@gmail.com>
2018-04-06 08:49:19 +0200

    Add icons in schedule talks cards

--- src/app/schedule/src/components/schedule-talk/schedule-talk.component.ts
+++ src/app/schedule/src/components/schedule-talk/schedule-talk.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { FavoriteTalksService } from '../../../../services/favorites.service';
 import { Talk } from '../../../../models/talk';
+import { environment } from '../../../../../environments/environment'
 
 @Component({
     selector: 'app-schedule-talk',
@@ -11,6 +12,7 @@ export class ScheduleTalkComponent implements OnInit {
 
     @Input('talk') talk: Talk;
     favorited: boolean;
+    env = environment;
 
     constructor(private favorites: FavoriteTalksService) { }
 
