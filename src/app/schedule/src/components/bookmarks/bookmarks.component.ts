3044d85225ce68ba0062f541274d5735f33be902
Léo Donny <leo.donny2@gmail.com>
2018-04-06 09:32:25 +0200

    (front) fix bookmarks style

--- src/app/schedule/src/components/bookmarks/bookmarks.component.ts
+++ src/app/schedule/src/components/bookmarks/bookmarks.component.ts
@@ -5,50 +5,52 @@ import { Event } from '../../../../models/event';
 import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-bookmarks',
-  templateUrl: './bookmarks.component.html',
-  styleUrls: ['./bookmarks.component.css']
+    selector: 'app-bookmarks',
+    templateUrl: './bookmarks.component.html',
+    styleUrls: ['./bookmarks.component.scss']
 })
 export class BookmarksComponent implements OnInit {
 
-  public eventsData: Event[];
-  public schedule: any;
+    public eventsData: Event[];
+    public bookmark: any;
+    public isBookmarkResolved: boolean;
 
-  constructor(private eventsService: EventsService, private favorite: FavoriteTalksService) {
-      this.schedule = {};
-  }
+    constructor(private eventsService: EventsService, private favorite: FavoriteTalksService) {
+        this.bookmark = {};
+        this.isBookmarkResolved = false;
+    }
 
-  sortEvents(eventsData): object {
-      const tmpSchedule = {};
+    sortEvents(eventsData): object {
+        const tmpSchedule = {};
 
-      eventsData.map((event) => {
-          if (!tmpSchedule.hasOwnProperty(event.day)) {
-              tmpSchedule[event.day] = [];
-          }
+        eventsData.map((event) => {
+            if (!tmpSchedule.hasOwnProperty(event.day)) {
+                tmpSchedule[event.day] = [];
+            }
 
-          if (!_.findKey(tmpSchedule[event.day], { time: event.fromTime })) {
-              const tmpPeriode = {};
-              tmpPeriode[event.fromTime] = [];
-              tmpSchedule[event.day].push({ time: event.fromTime, events: [] });
-          }
+            if (!_.findKey(tmpSchedule[event.day], { time: event.fromTime })) {
+                const tmpPeriode = {};
+                tmpPeriode[event.fromTime] = [];
+                tmpSchedule[event.day].push({ time: event.fromTime, events: [] });
+            }
 
-          _.forEach(tmpSchedule[event.day], (periode) => {
-              if (periode.time === event.fromTime) {
-                  periode.events.push(event);
-              }
-          });
-      });
+            _.forEach(tmpSchedule[event.day], (periode) => {
+                if (periode.time === event.fromTime) {
+                    periode.events.push(event);
+                }
+            });
+        });
 
-      return tmpSchedule;
-  }
+        return tmpSchedule;
+    }
 
-  ngOnInit() {
-    const bookmarks = this.favorite.getFavorites();
-      this.eventsService.getEventsWithIds(bookmarks).subscribe((data) => {
-          this.eventsData = data;
-          this.schedule = this.sortEvents(this.eventsData);
-          console.log('bookmark', this.schedule);
-      });
+    ngOnInit() {
+        const bookmarks = this.favorite.getFavorites();
+        this.eventsService.getEventsWithIds(bookmarks).subscribe((data) => {
+            this.eventsData = data;
+            this.bookmark = this.sortEvents(this.eventsData);
+            this.isBookmarkResolved = true;
+        });
 
-  }
+    }
 }
