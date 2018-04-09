d4bed694955bdbfbffd1c0f029e82a3f95321666
Léo Donny <leo.donny2@gmail.com>
2018-04-09 16:13:39 +0200

    fix tiny flag

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
   flags: {
     fr: '/assets/flag_fr.png',
     en: '/assets/flag_en.png',
-    tiny_fr: '/assets/tinyflag_fr.png',
+    tiny_fr: '/assets/tiny_flag_fr.png',
     tiny_en: '/assets/tiny_flag_en.png'
   },
   menu: [
