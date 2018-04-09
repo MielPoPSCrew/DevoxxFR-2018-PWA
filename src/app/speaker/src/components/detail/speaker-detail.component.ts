73ff56c0956015a853b404771af6316743535d7e
Léo Donny <leo.donny2@gmail.com>
2018-04-09 15:13:11 +0200

    (front) talks-list element css = talks in speaker-detail css

--- src/app/speaker/src/components/detail/speaker-detail.component.ts
+++ src/app/speaker/src/components/detail/speaker-detail.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
 import { environment } from '../../../../../environments/environment';
+
+// Models
+import { Talk } from '../../../../models/talk';
 import { Speaker } from '../../../../models/speaker';
 
 // Services
 import { EventsService } from '../../../../services/events.service';
 import { SpeakersService } from '../../../../services/speakers.service';
-
 
 @Component({
     selector: 'app-speaker',
@@ -18,12 +21,15 @@ export class SpeakerDetailComponent implements OnInit {
     displayedColumns = ['type', 'title'];
     env = environment;
 
+    public talks: Talk[];
+    public talksData = new MatTableDataSource();
     public speaker: Speaker;
 
     constructor(
         private activatedRoute: ActivatedRoute,
         private speakersService: SpeakersService,
         private eventsService: EventsService) {
+            this.talks = [];
     }
 
     ngOnInit() {
@@ -33,6 +39,15 @@ export class SpeakerDetailComponent implements OnInit {
 
             this.speakersService.getSpeaker(speakerId).subscribe((data) => {
                 this.speaker = data;
+
+                this.speaker.acceptedTalks.map((talk) => {
+                    this.eventsService.getTalk(talk.id).subscribe((fullEvent) => {
+                        console.log(fullEvent.talk);
+                        this.talks.push(fullEvent.talk);
+                        this.talksData.data = this.talks;
+                    });
+                });
+
                 console.log(this.speaker);
             });
         });
