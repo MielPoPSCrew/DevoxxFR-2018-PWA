871692070eb329b88f16ab7e217f83ac0bae599b
Léo Donny <leo.donny2@gmail.com>
2018-04-19 11:36:19 +0200

    Version 1.1 - > 1.2 (#19)
    * Add preprod Firebase environment and update travis config
    
    * Change travis configuration to run on develop
    
    * (#5) Images in talk detail are now a max-width and an auto height
    
    * (#8) Add the day of the event in talk detail
    
    * Update README.md
    
    * Bugfix/#7 navigation lost (#12)
    
    * Saving scroll position when using back/next navigation buttons
    
    * Saving tab index on schedule / bookmarks page
    
    * Adding Google fonts to SW cache (#11)
    
    * (audit) improve accessibilty score (#13)
    
    * (front) hotfix css home
    
    * version : 1.0.0 -> 1.1.0
    
    * hotfix: orientation : portrait
    
    * (hotfix) fix target of about link
    
    * (fix) fix twitter links : clean them and remove "@" (#17)
    
    * (version) v1.1.0 -> v1.2.0

--- src/app/speaker/src/components/detail/speaker-detail.component.ts
+++ src/app/speaker/src/components/detail/speaker-detail.component.ts
@@ -24,12 +24,14 @@ export class SpeakerDetailComponent implements OnInit {
     public talks: Talk[];
     public talksData = new MatTableDataSource();
     public speaker: Speaker;
+    public speakerTwitterAt: string;
 
     constructor(
         private activatedRoute: ActivatedRoute,
         private speakersService: SpeakersService,
         private eventsService: EventsService) {
             this.talks = [];
+            this.speakerTwitterAt = '';
     }
 
     ngOnInit() {
@@ -38,6 +40,10 @@ export class SpeakerDetailComponent implements OnInit {
 
             this.speakersService.getSpeaker(speakerId).subscribe((data) => {
                 this.speaker = data;
+
+                if (this.speaker.twitter) {
+                    this.speakerTwitterAt = this.cleanTwitterField(data.twitter);
+                }
 
                 this.speaker.acceptedTalks.map((talk) => {
                     this.eventsService.getTalk(talk.id).subscribe((fullEvent) => {
@@ -51,5 +57,16 @@ export class SpeakerDetailComponent implements OnInit {
 
     errorHandler(event) {
         event.target.src = '/assets/default_black.png';
+    }
+
+    cleanTwitterField(twitter) {
+        let cleanTwitterAt = twitter.replace(/@/g, '');
+
+        if (cleanTwitterAt.includes('http')) {
+            const splitStr = cleanTwitterAt.split('/');
+            cleanTwitterAt = splitStr[splitStr.length - 1];
+        }
+
+        return cleanTwitterAt;
     }
 }
